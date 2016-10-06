ea3b577d197a693a839400fb960241c9eebadc6e
SoUnd001 <raduraducu200@gmail.com>
2016-10-06 18:28:43 +0300

    FIx camera ( try )

--- include/camera/CameraParametersExtra.h
+++ include/camera/CameraParametersExtra.h
@@ -15,11 +15,59 @@
  */
 
 #define CAMERA_PARAMETERS_EXTRA_C \
-const char CameraParameters::KEY_CAMERA_MODE[] = "camera-mode"; \
-const char CameraParameters::KEY_ISO_MODE[] = "iso"; \
-const char CameraParameters::KEY_SUPPORTED_ISO_MODES[] = "iso-values";
+const char CameraParameters::KEY_DIS[] = "dis"; \
+const char CameraParameters::DIS_DISABLE[] = "disable"; \
+\
+const char CameraParameters::KEY_DYNAMIC_RANGE_CONTROL[] = "dynamic-range-control"; \
+const char CameraParameters::KEY_SUPPORTED_DYNAMIC_RANGE_CONTROL[] = "dynamic-range-control-values"; \
+const char CameraParameters::DRC_ON[] = "on"; \
+const char CameraParameters::DRC_OFF[] = "off"; \
+\
+const char CameraParameters::KEY_PHASE_AF[] = "phase-af"; \
+const char CameraParameters::KEY_SUPPORTED_PHASE_AF[] = "phase-af-values"; \
+const char CameraParameters::PAF_ON[] = "on"; \
+const char CameraParameters::PAF_OFF[] = "off"; \
+\
+const char CameraParameters::KEY_RT_HDR[] = "rt-hdr"; \
+const char CameraParameters::KEY_SUPPORTED_RT_HDR[] = "rt-hdr-values"; \
+const char CameraParameters::RTHDR_ON[] = "on"; \
+const char CameraParameters::RTHDR_OFF[] = "off"; \
+\
+const char CameraParameters::KEY_ZSL[] = "zsl"; \
+const char CameraParameters::ZSL_ON[] = "on"; \
+const char CameraParameters::ZSL_OFF[] = "off"; \
+\
+int CameraParameters::getInt64(__attribute__((__unused__)) const char *key) const { return -1; } \
+\
+/* LAST_LINE OF CAMERA_PARAMETERS_EXTRA_C, every line before this one *MUST* have
+ * a backslash \ at the end of the line or else everything will break.
+ */
 
 #define CAMERA_PARAMETERS_EXTRA_H \
-    static const char KEY_CAMERA_MODE[]; \
-    static const char KEY_ISO_MODE[]; \
-    static const char KEY_SUPPORTED_ISO_MODES[];
+    static const char KEY_DIS[]; \
+    static const char DIS_DISABLE[]; \
+    \
+    static const char KEY_DYNAMIC_RANGE_CONTROL[]; \
+    static const char KEY_SUPPORTED_DYNAMIC_RANGE_CONTROL[]; \
+    static const char DRC_ON[]; \
+    static const char DRC_OFF[]; \
+    \
+    static const char KEY_PHASE_AF[]; \
+    static const char KEY_SUPPORTED_PHASE_AF[]; \
+    static const char PAF_ON[]; \
+    static const char PAF_OFF[]; \
+    \
+    static const char KEY_RT_HDR[]; \
+    static const char KEY_SUPPORTED_RT_HDR[]; \
+    static const char RTHDR_ON[]; \
+    static const char RTHDR_OFF[]; \
+    \
+    static const char KEY_ZSL[]; \
+    static const char ZSL_ON[]; \
+    static const char ZSL_OFF[]; \
+    \
+    int getInt64(const char *key) const; \
+    \
+    /* LAST_LINE OF CAMERA_PARAMETERS_EXTRA_H, every line before this one *MUST* have
+     * a backslash \ at the end of the line or else everything will break.
+     */
